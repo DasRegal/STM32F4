f5fc128bcc4aacc27e73674b8e701fba7cf87f26
Ruslan <plexus.9310@gmail.com>
2020-03-11 12:35:34 +0700

    feat: add optimisation flags

--- cmake/stm32f429zit.cmake
+++ cmake/stm32f429zit.cmake
@@ -1,4 +1,5 @@
 set(COMMON_FLAGS "-mcpu=cortex-m4 -mthumb")
+set(COMMON_FLAGS "${COMMON_FLAGS} -ffunction-sections -fdata-sections")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMMON_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMMON_FLAGS}")
 
