3c6011814d967865b6723635ec92b31ee925d567
Ruslan <plexus.9310@gmail.com>
2020-03-11 12:36:56 +0700

    chore: add GPIO control

--- src/main.c
+++ src/main.c
@@ -2,6 +2,17 @@
 
 int main(void)
 {
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_13;
+	GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Low_Speed;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+	GPIO_SetBits(GPIOG, GPIO_Pin_13);
     while (1)
     {
     }
